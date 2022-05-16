a8cfb30fdb23b8f7b8903678afaedfd15f0f3fdd
pbuba <sas_bub@mail.ru>
2022-05-16 20:07:57 +0300

    Initial Commit

---
+++ git-task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  git-task
+//
+//  Created by Павел Бубликов on 16.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
